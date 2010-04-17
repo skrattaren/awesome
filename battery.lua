f529557c644fbd9ae53107720a38b2b5218cd0df
Nickolaj Stjujsckij <sterkrig@home.no>
2010-04-17 13:32:57 +0400

    @: closure in battery widget

--- battery.lua
+++ battery.lua
@@ -3,44 +3,80 @@ local math = math
 local naughty = naughty
 local beautiful = beautiful
 local tonumber = tonumber
+local tostring = tostring
+local print = print
+local pairs = pairs
 local coroutine = coroutine
 
 module("battery")
 -- Based on http://awesome.naquadah.org/wiki/Gigamo_Battery_Widget
 
-function batteryInfo(adapter)
-    spacer = " "
-    local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")    
+local limits = {{25, 5},
+          {12, 3},
+           {7, 1},
+            {0}}
+
+function get_bat_state(adapter)
+    local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")
     local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
     local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
     local cur = fcur:read()
     local cap = fcap:read()
     local sta = fsta:read()
+    fcur:close()
+    fcap:close()
+    fsta:close()
     local battery = math.floor(cur * 100 / cap)
-    local prefix = "⚡"
-
     if sta:match("Charging") then
-        dir = "↑"
+        dir = 1
     elseif sta:match("Discharging") then
-        dir = "↓"
-        prefix = "Bat:"
-        if tonumber(battery) < 25 then
-            naughty.notify({title = "⚡ Lystring! ⚡",
+        dir = -1
+    else
+        dir = 0
+        battery = ""
+    end
+    return battery, dir
+end
+
+function getnextlim (num)
+    for ind, pair in pairs(limits) do
+        lim = pair[1]; step = pair[2]; nextlim = limits[ind+1][1] or 0
+        if num > nextlim then
+            repeat
+                lim = lim - step
+            until num > lim
+            if lim < nextlim then
+                lim = nextlim
+            end
+            return lim
+        end
+    end
+end
+
+
+function batclosure(adapter)
+    local nextlim = limits[1][1]
+    return function ()
+        local prefix = "⚡"
+        local battery, dir = get_bat_state(adapter)
+        if dir == -1 then
+            dirsign = "↓"
+            prefix = "Batt:"
+            if battery <= nextlim then
+                naughty.notify({title = "⚡ Lystring! ⚡",
                             text = "Batteriet har nått låg nivå ( ⚡ "..battery.."%)!",
                             timeout = 7,
                             position = "bottom_right",
                             fg = beautiful.fg_focus,
                             bg = beautiful.bg_focus
                             })
+                nextlim = getnextlim(battery)
+            end
+        elseif dir == 1 then
+            dirsign = "↑"
         end
-    else
-        dir = ""
-        battery = ""
+        if dir ~= "" then battery = battery.."%" end
+        return " "..prefix.." "..dirsign..battery..dirsign.." "
     end
-    if dir ~= "" then battery = battery.."%" end
-    fcur:close()
-    fcap:close()
-    fsta:close()
-    return spacer..prefix..spacer..dir..battery..dir..spacer
 end
 
